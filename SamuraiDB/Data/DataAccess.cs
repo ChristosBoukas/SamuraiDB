e66e381c83a6fa147e736a41eb759da6ed0103c7
Christos Boukas <Christosboukast@gmail.com>
2023-12-01 14:54:05 +0100

    added try catch to dataaccess.seed method

--- SamuraiDB/Data/DataAccess.cs
+++ SamuraiDB/Data/DataAccess.cs
@@ -16,65 +16,74 @@ namespace SamuraiDB.Data
         {
             using (Context context = new Context())
             {
-                var rnd = new csSeedGenerator();
-
-                List<Samurai> samuraiList = new List<Samurai>();
-                List<Battle> battleList = new List<Battle>();
-
-                for (int i = 0; i < 50; i++)
+                try
                 {
-                    Samurai seededSamurai = new Samurai();
-                    seededSamurai.FirstName = rnd.FirstName;
-                    seededSamurai.LastName = rnd.LastName;
-                    seededSamurai.Hairstyle = rnd.FromEnum<enHairstyle>();
-                    samuraiList.Add(seededSamurai);
-                }
+                    var rnd = new csSeedGenerator();
 
-                for (int i = 0; i < 10; i++)
-                {
-                    Battle seededBattle = new Battle();
-                    seededBattle.Name = rnd.City();
-                    seededBattle.Description = "Some rand desc";
-                    seededBattle.wasBrutal = rnd.Bool;
-                    seededBattle.StartDate = rnd.DateAndTime(1200, 1400);
-                    seededBattle.EndDate = rnd.DateAndTime(1500, 1600);
-                    battleList.Add(seededBattle);
-                }
+                    List<Samurai> samuraiList = new List<Samurai>();
+                    List<Battle> battleList = new List<Battle>();
+
+                    for (int i = 0; i < 50; i++)
+                    {
+                        Samurai seededSamurai = new Samurai();
+                        seededSamurai.FirstName = rnd.FirstName;
+                        seededSamurai.LastName = rnd.LastName;
+                        seededSamurai.Hairstyle = rnd.FromEnum<enHairstyle>();
+                        samuraiList.Add(seededSamurai);
+                    }
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Battle seededBattle = new Battle();
+                        seededBattle.Name = rnd.City();
+                        seededBattle.Description = "Some rand desc";
+                        seededBattle.wasBrutal = rnd.Bool;
+                        seededBattle.StartDate = rnd.DateAndTime(1200, 1400);
+                        seededBattle.EndDate = rnd.DateAndTime(1500, 1600);
+                        battleList.Add(seededBattle);
+                    }
 
 
-                int samuraiIndex = 0;
+                    int samuraiIndex = 0;
 
-                // Iterate through the battle list
-                while (samuraiList.Count > 0)
-                {
+                    // Iterate through the battle list
+                    while (samuraiList.Count > 0)
+                    {
+                        foreach (Battle battle in battleList)
+                        {
+                            if (samuraiList.Count == 0)
+                            {
+                                break; // Break if there are no more samurai left
+                            }
+
+                            // Add one samurai to the current battle
+                            battle.Samurais.Add(samuraiList[0]);
+                            samuraiList.RemoveAt(0); // Remove the added samurai from the samurai list
+                        }
+                    }
+
+                    context.SaveChanges();
+
+
                     foreach (Battle battle in battleList)
                     {
-                        if (samuraiList.Count == 0)
+                        Console.WriteLine($"Battle: {battle.Name}");
+                        Console.WriteLine($"Samurais:");
+                        foreach (Samurai samurai in battle.Samurais)
                         {
-                            break; // Break if there are no more samurai left
+                            Console.WriteLine($"{samurai.FirstName} {samurai.LastName}");
                         }
+                        Console.WriteLine();
 
-                        // Add one samurai to the current battle
-                        battle.Samurais.Add(samuraiList[0]);
-                        samuraiList.RemoveAt(0); // Remove the added samurai from the samurai list
                     }
+
                 }
-
-                context.SaveChanges();
-
-                foreach (Battle battle in battleList)
+                catch (Exception ex)
                 {
-                    Console.WriteLine($"Battle: {battle.Name}");
-                    Console.WriteLine($"Samurais:");
-                    foreach (Samurai samurai in battle.Samurais)
-                    {
-                        Console.WriteLine($"{samurai.FirstName} {samurai.LastName}");
-                    }
-                    Console.WriteLine();
-
+                    Console.WriteLine($"Error saving changes: {ex.Message}");
                 }
 
-                
+
 
             }
         }
